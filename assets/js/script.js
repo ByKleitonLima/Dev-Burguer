42b2a0639d3eb7e9f13b5413d3d1a12e0998b83e
Kleiton Lima <k.oliveira2002@outlook.com>
2025-07-18 00:35:58 -0300

    blocking desktop access

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Acesso a dispositivos moveis
-/*
+
 function isMobile() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
@@ -8,9 +8,8 @@ if (!isMobile()) {
     document.body.innerHTML = `
         <div class="flex items-center justify-center min-h-screen bg-[#3233232]">
         <div class="text-center p-4">
-            <h1 class="text-2xl font-bold text-[#AAAAAA] mb-4"> Acesse negado </h1>
-            <p class="text-[#AAAAAA]">Só é possível acessar pelo dispositivo móvel.</p>
+            <h1 class="text-[40px] font-bold text-[#F86106] mb-4"> Acesse negado </h1>
+            <p class="text-[20px]"> Só é possível acessar pelo dispositivo móvel. </p>
         </div>
         </div>`;
 }
-*/
