2e24c5495905d1caaec006169cea6097ec08e992
Kleiton Lima <k.oliveira2002@outlook.com>
2025-07-18 23:56:21 -0300

    new section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 // Acesso a dispositivos moveis
 
-function isMobile() {
+function isMobileOrTablet() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-if (!isMobile()) {
+if (!isMobileOrTablet()) {
     document.body.innerHTML = `
-        <div class="flex items-center justify-center min-h-screen bg-[#3233232]">
-        <div class="text-center p-4">
-            <h1 class="text-[40px] font-bold text-[#F86106] mb-4"> Acesse negado </h1>
-            <p class="text-[20px]"> Só é possível acessar pelo dispositivo móvel. </p>
+        <div class="flex items-center justify-center min-h-screen bg-[#F0EDD8]">
+            <div class="text-center p-4">
+                <h1 class="text-[40px] font-bold text-[#F86106] mb-4">Acesso negado</h1>
+                <p class="text-[20px]">Só é possível acessar pelo dispositivo móvel ou tablet.</p>
+            </div>
         </div>
-        </div>`;
+    `;
 }
+
+
